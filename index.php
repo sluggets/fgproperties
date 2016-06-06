9598ed3af3a3a4e44910f4064e731093a0f84e1f
timothy <tim.longoria@gmail.com>
2016-06-06 18:44:46 -0500

    Begins php templates for footer, index, and header

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>First Generation Properties</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/fgprop.css" rel="stylesheet">
-    <script src="js/jquery.min.js"></script>
-    <script src="js/fgprop.js"></script>
-  </head>
-  <body>
-  </body>
-</html>
+<?php
+    include 'header.php';
+?>
+
+
+
+<?php
+    include 'footer.php';
+?>
