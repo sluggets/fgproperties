208768f7de8542c4a16880060b7f128b68d0502e
timothy <tim.longoria@gmail.com>
2016-06-07 14:20:51 -0500

    Adds footer data, social network links, placeholder links

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
-      <footer>
+      <footer class="footer navbar-fixed-bottom">
+        <div class="container footer-margin">
+          <ul class="nav flat">
+            <li><p class="text-muted">Sticky footer here</p></li>
+            <li><div class="fb-share-button" data-href="http://firstgenproperties.rent" data-layout="button_count" data-mobile-iframe="true"></div></li>
+            <li><a href="https://twitter.com/intent/tweet?text=Check%20it%20out!%20http%3A%2F%2Ffirstgenproperties.rent"> Tweet<i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></li>
+            <li><p class="text-muted">More stuff</p></li>
+            <li><p class="text-muted">Developed by Tim</p></li>
+            <li><p class="text-muted">Contact</p></li>
+          </ul>
+        </div>
       </footer>
     </div>
   </body>
