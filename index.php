7fcc65eef5d17ae24b7759dcef36d555f1960a09
timothy <tim.longoria@gmail.com>
2016-06-08 14:13:16 -0500

    Begins landing page properties widgets

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
     include 'header.php';
 ?>
-        <div id="logo-img">
-          <img src="img/fgp-logo-trans-300.png" alt="First Generation Properties Logo">
-          <h1>Welcome to First Generation Properties</h1>
-          <p>Click on a property to view available units.</p>
+        <div id="clear-footer">
+          <div id="logo-img">
+            <img src="img/fgp-logo-trans-300.png" alt="First Generation Properties Logo">
+            <h1>Welcome to First Generation Properties</h1>
+            <p>Click on a property to view available units.</p>
+          </div>
+          <div class="container-fluid">
+            <div id="row">
+              <div class="col-md-12 town-header">
+                <h2 id="annawan">Annawan, IL</h2>
+              </div>
+            </div>
+            <div id="row">
+              <div class="col-md-2 col-md-offset-5">
+                <div class="thumbnail">
+                  <img src="img/ph01.jpg" alt="House at 203 N. State Street">
+                  <div class="caption">
+                    <h3>4 Plex<address> 203 N. State Street Annawan, IL</address></h3>
+                    <p>Four units, one or two bedrooms with one bath. Click through for more details.</p>
+                    <p><a href="#" class="btn btn-default" role="button">More information</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 <?php
     include 'footer.php';
