293af6361e001ee82d8ca768be5da7775d2892a3
timothy <tim.longoria@gmail.com>
2016-06-08 15:43:51 -0500

    Adds remainder of property thumbnails

--- index.php
+++ index.php
@@ -1,32 +1,138 @@
 <?php
     include 'header.php';
 ?>
-        <div id="clear-footer">
-          <div id="logo-img">
-            <img src="img/fgp-logo-trans-300.png" alt="First Generation Properties Logo">
-            <h1>Welcome to First Generation Properties</h1>
-            <p>Click on a property to view available units.</p>
-          </div>
-          <div class="container-fluid">
-            <div id="row">
-              <div class="col-md-12 town-header">
-                <h2 id="annawan">Annawan, IL</h2>
-              </div>
+          <div id="clear-footer">
+            <div id="logo-img">
+              <img src="img/fgp-logo-trans-300.png" alt="First Generation Properties Logo">
+              <h1>Welcome to First Generation Properties</h1>
+              <p>Click on a property to view available units.</p>
             </div>
-            <div id="row">
-              <div class="col-md-2 col-md-offset-5">
-                <div class="thumbnail">
-                  <img src="img/ph01.jpg" alt="House at 203 N. State Street">
-                  <div class="caption">
-                    <h3>4 Plex<address> 203 N. State Street Annawan, IL</address></h3>
-                    <p>Four units, one or two bedrooms with one bath. Click through for more details.</p>
-                    <p><a href="#" class="btn btn-default" role="button">More information</a>
+            <div class="container-fluid">
+              <div id="row">
+                <div class="col-md-12 town-header">
+                  <h2 id="annawan">Annawan, IL</h2>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-2 col-md-offset-5">
+                  <div class="thumbnail">
+                    <img src="img/ph01.jpg" alt="House at 203 N. State Street">
+                    <div class="caption">
+                      <h3>4 Plex<address> 203 N. State Street Annawan, IL</address></h3>
+                      <p>Four units: one or two bedrooms with one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
                   </div>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-12 town-header">
+                  <h2 id="cambridge">Cambridge, IL</h2>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-2 col-md-offset-5">
+                  <div class="thumbnail">
+                    <img src="img/ph02.jpg" alt="House at 555 S. Need Street">
+                    <div class="caption">
+                      <h3>3 Plex<address> 555 S. Need Street Cambridge, IL</address></h3>
+                      <p>Three units: one, two or three bedrooms with one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-12 town-header">
+                  <h2 id="geneseo">Geneseo, IL</h2>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-4">
+                </div>
+                <div class="col-md-2">
+                  <div class="thumbnail">
+                    <img src="img/ph03.jpg" alt="House at 612 S. Spring Street">
+                    <div class="caption">
+                      <h3>3 Plex<address> 612-616 S. Spring Street Geneseo, IL</address></h3>
+                      <p>Three units: two or three bedrooms with one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="thumbnail">
+                    <img src="img/ph04.jpg" alt="House at 823 S. Spring Street">
+                    <div class="caption">
+                      <h3>Single Family<address> 823 S. Spring Street Geneseo, IL</address></h3>
+                      <p>Single family home: two bedrooms and two baths. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-12 town-header">
+                  <h2 id="leclaire">LeClaire, IA</h2>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-2 col-md-offset-5">
+                  <div class="thumbnail">
+                    <img src="img/ph05.jpg" alt="House at 1478 Kaylann Avenue">
+                    <div class="caption">
+                      <h3>Single Family<address> 1478 Kaylann Avenue LeClaire, IA</address></h3>
+                      <p>Single family home: three to four bedrooms, and 1&#189 baths.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-12 town-header">
+                  <h2 id="moline">Moline, IL</h2>
+                </div>
+              </div>
+              <div id="row">
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-2">
+                  <div class="thumbnail">
+                    <img src="img/ph06.jpg" alt="Building at 2824 44th Street">
+                    <div class="caption">
+                      <h3>4 Plex<address> 2824 44th Street Moline, IL</address></h3>
+                      <p>Fourunits: one to four bedrooms with one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="thumbnail">
+                    <img src="img/ph07.jpg" alt="House at 116 18th Avenue">
+                    <div class="caption">
+                      <h3>Single Family<address> 116 18th Ave Moline, IL</address></h3>
+                      <p>Single family home: two bedrooms and one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="thumbnail">
+                    <img src="img/ph08.jpg" alt="House at 123 18th Avenue">
+                    <div class="caption">
+                      <h3>Single Family<address> 123 18th Avenue Moline, IL</address></h3>
+                      <p>Single family home: three bedrooms and one bath. Click through for more details.</p>
+                      <p><a href="#" class="btn btn-default" role="button">More information</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-3">
                 </div>
               </div>
             </div>
           </div>
-        </div>
 <?php
     include 'footer.php';
 ?>
