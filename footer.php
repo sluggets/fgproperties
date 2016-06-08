d873f2844df47f6d4a75650c8ed82b8c112849fb
timothy <tim.longoria@gmail.com>
2016-06-08 14:11:55 -0500

    Changes minor style rules, footer elements

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             <li class="social"><div class="social-circle"><a href="https://twitter.com/intent/tweet?text=Check%20it%20out!%20http%3A%2F%2Ffirstgenproperties.rent"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></div></li>
             <li class="social"><div class="social-circle"><a href="http://www.facebook.com/share.php?u=http%3A%2F%2Ffirstgenproperties.rent&title=First%20Generation%20Properties"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></div></li>
           </ul>
-          <p id="address">First Generation Properties, LLC P.O. Box 493 Geneseo, IL 61254</p>
+          <address id="address">First Generation Properties, LLC P.O. Box 493 Geneseo, IL 61254</address>
           <p id="email"><a href="mailto:firstgenprop@gmail.com">firstgenprop@gmail.com</a></p>
         </div>
       </footer>
