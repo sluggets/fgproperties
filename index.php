8f64bda4277a46c24f2e2035cbba9260a5423207
timothy <tim.longoria@gmail.com>
2016-06-09 12:47:58 -0500

    Changes sub-heading on landing page

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <div id="logo-img">
               <img src="img/fgp-logo-trans-300.png" alt="First Generation Properties Logo">
               <h1>Welcome to First Generation Properties</h1>
-              <p>Click on a property to view available units.</p>
+              <p>Click to view units.</p>
             </div>
             <div class="container-fluid">
               <div id="row">
