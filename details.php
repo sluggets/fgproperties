de34ba483769dcd237c74d9dc53d51f9f28b3816
timothy <tim.longoria@gmail.com>
2016-06-10 16:35:32 -0500

    Sets up variables in for loop, sets up stats panel in details

--- details.php
+++ details.php
@@ -28,21 +28,44 @@
 
   include $property . '-descriptions.php';
 
+  
   $listing_len = count($listing_array);
 
   for ($i = 0; $i < $listing_len; $i++)
   {
+    $address = $listing_array[$i]->address;
+    $beds = $listing_array[$i]->beds;
+    $baths = $listing_array[$i]->baths;
+    $extra = $listing_array[$i]->extra;
+    $aptNum = $listing_array[$i]->aptNumber;
+    $path = $listing_array[$i]->photoPath;
 ?>
     <div id="row">
       <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
             <h2 class="panel-title">
-              <?php echo $listing_array[$i]->address; ?>
-              <span id="apt"><?php if ($listing_array[$i]->aptNumber){echo 'APT' . $listing_array[$i]->aptNumber;} ?></span>
+              <?php echo $address; ?>
+              <span id="apt"><?php if ($aptNum){echo 'APT ' . $aptNum;} ?></span>
             </h2>
           </div>
+          <div class="panel-heading">
+            <h4>
+              <span class="stats">
+                Beds: <?php echo $beds; ?>
+              </span>
+              <span class="stats">
+                Baths: <?php echo $baths; ?>
+              </span>
+              <span class="stats">
+                <?php if($extra){ echo 'Additional: ' . $extra;} ?>
+              </span>
+            </h4>
+          </div>
           <div class="panel-body">
+          </div>
+          <div class="panel-footer">
+            <h4>Map goes here</h4>
           </div>
         </div>
       </div>
