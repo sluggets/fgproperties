f92264572fd135d1058b1b6a027b4dc9b0c7b5ef
Tim Longoria <tim.longoria@gmail.com>
2018-08-19 08:00:19 -0400

    Minor grammatical fixes

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
           <ul class="nav flat">
             <li><p class="text-muted">About Us</p></li>
             <li><p class="text-muted"><a href="mailto:firstgenprop@gmail.com">Contact</a></p></li>
-            <li class="social"><div class="social-circle"><a href="https://twitter.com/intent/tweet?text=Check%20it%20out!%20http%3A%2F%2Ffirstgenproperties.rent"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></div></li>
+            <li class="social"><div class="social-circle"><a href="https://twitter.com/intent/tweet?text=Check%20it%20out!%20http%3A%2F%2Ffirstgenproperties.com"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></div></li>
             <li class="social"><div class="social-circle"><a href="http://www.facebook.com/share.php?u=http%3A%2F%2Ffirstgenproperties.rent&title=First%20Generation%20Properties"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></div></li>
           </ul>
           <address id="address">First Generation Properties, LLC P.O. Box 493 Geneseo, IL 61254</address>
--- leclaire-1-descriptions.php
+++ leclaire-1-descriptions.php
@@ -2,7 +2,7 @@
       // builds an Listing object for each unit at this property
   $leclaire1_1 = new Listing;
   $leclaire1_1->add_item('1478 Kaylann Dr, LeClaire, IA 52753', '3-4', '1&#189', 0, 0, 'img/leclaire-1/', [41.59334, -90.36171]);
-  $leclaire1_1->description = 'Three to four bedroom, 1 &#189 bath, single family home with detached garage and washer/dryer hookup. Central A/C in a family friendly LeClaire neighborhood. Utilities extra. Pets welcom but additional.';
+  $leclaire1_1->description = 'Three to four bedroom, 1 &#189 bath, single family home with detached garage and washer/dryer hookup. Central A/C in a family friendly LeClaire neighborhood. Utilities extra. Pets welcome but additional.';
 
   // builds array of Listing objecs to iterate over when 
   // imported into details.php
